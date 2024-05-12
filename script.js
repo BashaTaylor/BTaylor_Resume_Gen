a04e703de76fbe42786573e1208a5018bfef3e46
Barbara Taylor <83436169+BashaTaylor@users.noreply.github.com>
2024-05-12 19:42:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,8 @@
-function addComment() {
-    // Get the message of the new Comment
-    let comment = document.getElementById("new_comment");
-    // If the user has left a comment, display a po-up
-    if (comment.value != null && comment.value.trim() != "") {
-        console.log("New comment added");
-        // Call showPopup here
-        showPopup(true);
-
-
-// Create a new 'comment' element and set it's value to the user's message
-var element = document.createElement("div");
-element.setAttribute("class", "comment");
-element.innerHTML = "\<span\>&#8220;\</span\>" + comment.value + "\<span\>&#8221;\</span\>";
-// Add this element to the end of the list of comments
-document.getElementById("all_comments").appendChild(element);
-
-// Reset the value of the textarea
-comment.value = "";
-    }
+function showPopup() {
+    var popup = document.getElementById("To print in correct scale, go to: More Settings, change Scale from default to Custom, and select 45%");
+    popup.style.visibility = "visible";
 }
 
-function showPopup(bool) {
-    if (bool) {
-        document.getElementById('popup').style.visibility = 'visible'
-
-    }   else {
-        document.getElementById('popup').style.visibility = 'hidden'
-    }
+function printPage() {
+    window.print();
 }
-
